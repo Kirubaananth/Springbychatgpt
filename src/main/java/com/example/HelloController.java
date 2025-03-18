609dd1dbe647ea6330e6376f0db2c41146376258
Kirubaananth <kirubaananth0725@gmail.com>
2025-03-18 20:00:11 +0530

    Update to jsn

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,11 @@
 package com.example;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
@@ -9,5 +13,18 @@ public class HelloController {
 	public String sayHello()
 	{
 		return "hello,springboot";
+	}
+	@GetMapping("/hello/{name}")
+	public String greetUser(@PathVariable String name)
+	{
+		return "Hello, "+name+"!";
+	}
+	@GetMapping("/user")
+	public Map<String,String> getUser()
+	{
+		Map<String,String> user=new HashMap<>();
+		user.put("name","sanjay");
+		user.put("email", "sanjay@gmail.com");
+		return user;
 	}
 }
